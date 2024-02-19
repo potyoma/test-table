e9b40e05044071f295e057d475f60b5e9dc81e79
Artem Novoselov <artyomnovosyolov254@gmail.com>
2024-02-19 14:12:16 +0100

    init: init project

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  safelist: ["border", "border-black", "border-collapse", "py-2", "px-4"],
+};
