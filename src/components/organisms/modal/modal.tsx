3c732bc1645305d191ea3437e0dca79b75c596a0
Artem Novoselov <artyomnovosyolov254@gmail.com>
2024-02-19 14:14:29 +0100

    feat: modal component

---
+++ src/components/organisms/modal/modal.tsx
@@ -0,0 +1,64 @@
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment, PropsWithChildren, ReactNode } from "react";
+import Button from "../../atoms/button";
+
+type ModalProps = PropsWithChildren & {
+  isOpen?: boolean;
+  onClose: () => void;
+  title?: ReactNode;
+};
+
+export function Modal({ isOpen, onClose, title, children }: ModalProps) {
+  return (
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={onClose}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="div"
+                    className="flex justify-between items-center"
+                  >
+                    {title}
+
+                    <Button
+                      onClick={onClose}
+                      className="bg-red-500 hover:bg-red-700"
+                    >
+                      <FontAwesomeIcon icon={faXmark} />
+                    </Button>
+                  </Dialog.Title>
+                  {children}
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
+}
